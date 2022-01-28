b8900d4b8b612f1ceff5f93dabe969855bd390e0
luismerizalde <luis.merizalde@ssc.com.co>
2022-01-28 08:30:00 -0500

    [DEV] desarrollo punto 2 parte 2

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,19 @@
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    """ funcion para crear, recuperar o editar la instancia de un modelo 
+        
+        :author: Desconocido
+
+        :param model(django.db.models.Model): instancia del modelo
+        :param data(dict): diccionario con los valores de los atributos
+            del modelo
+        :param id(str): id de la instancia
+        :param commit(bool): indica si los cambios se deben almacenar
+            en la base de datos
+
+        :return: instancia del modelo
+        :rtype: django.db.models.Model        
+    """
+
     if id:
         item = model.objects.get(id=id)
         try:
